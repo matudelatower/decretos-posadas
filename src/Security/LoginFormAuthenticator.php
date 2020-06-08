5b8ba83cc3e913f270e9a4621a98d6b2f32ab66c
Matias Solis de la Torre <matias.delatorre89@gmail.com>
2020-06-08 15:55:11 -0300

    fix todo

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -97,7 +97,6 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator implements P
         }
 
 //         For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
-//        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
 	    return new RedirectResponse($this->urlGenerator->generate('easyadmin'));
     }
 
